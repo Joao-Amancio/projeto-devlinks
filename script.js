fe37375facd5fea4ecb57f1797582c82d50b6b5a
= <=>
2024-03-21 15:43:34 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // if (html.classList.contains("light")) {
+  // html.classList.remove("light")
+  // } else {
+  //  html.classList.add("light")
+  // }
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, add  a imagem light
+    img.setAttribute("src", "./assets/Avatar.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+    // se tiver sem o light mode, manter a imagem normal
+  }
+}
